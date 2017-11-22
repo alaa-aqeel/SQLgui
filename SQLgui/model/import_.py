67e15a35c0226d7d482ec28c81b51bacfc78ab9c
Alaa Aqeel <alaa.21.iraq@gmail.com>
2017-11-22 04:52:31 +0300

    Update import_.py

--- SQLgui/model/import_.py
+++ SQLgui/model/import_.py
@@ -1,8 +1,20 @@
 try:
-	from PySide.QtGui import *
-	from PySide.QtCore import *
+	from PySide.QtGui import (
+				QMainWindow,QMessageBox,QFont,QAction,QIcon,
+				QDialog,QPushButton,QLabel,QGroupBox,QHBoxLayout,QCursor,
+				QCheckBox,QLineEdit,QTextEdit,QWidget,QPainter,QColor,QVBoxLayout,
+				QPlainTextEdit,QStringListModel,QTextCharFormat,QCompleter,QFontMetrics,
+				QTabWidget,QFileDialog,QMenuBar,QDockWidget,QTableWidget,QTextFormat,QSyntaxHighlighter,
+				)
+	from PySide.QtCore import Qt,QSize,QRegExp,SIGNAL,QRect
 except:
-	from PyQt4.QtGui import *
-	from PyQt4.QtCore import *
+	from PyQt4.QtGui import (
+				QMainWindow,QMessageBox,QFont,QAction,QIcon,
+				QDialog,QPushButton,QLabel,QGroupBox,QHBoxLayout,QCursor,
+				QCheckBox,QLineEdit,QTextEdit,QWidget,QPainter,QColor,QVBoxLayout,
+				QPlainTextEdit,QStringListModel,QTextCharFormat,QCompleter,QFontMetrics,
+				QTabWidget,QFileDialog,QMenuBar,QDockWidget,QTableWidget,QTextFormat,QSyntaxHighlighter,
+				)
+	from PyQt4.QtCore import Qt,QSize,QRegExp,SIGNAL,QRect
 
 import sqlite3,os,sys,re
