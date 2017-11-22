4bf2ab1cedff327a63d9529e7f4d54d620fe542f
Alaa Aqeel <alaa.21.iraq@gmail.com>
2017-11-22 04:25:56 +0300

    Create __init__.py

---
+++ SQLgui/gui/__init__.py
@@ -0,0 +1,13 @@
+# /*
+
+
+from .widget import dockEdit,Console,Table,MsgBox
+from .DConnect import DConnect
+from .menuBar  import menuBar
+from .Edit     import Edit
+from .about  import About
+
+# */
+
+#// 
+from .MainWindow  import Main
